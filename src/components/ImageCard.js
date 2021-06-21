c33e47a2f95b5e9fd3886d76d69b05caf8a8e8ce
aditya patel <patel.aditya2014@gmail.com>
2021-06-21 09:53:34 +0530

    adding image overlay to the images

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { AiOutlineLike, AiOutlineCloudDownload } from 'react-icons/ai';
 
 const ImageCard = ({ image }) => {
   return (
     <div key={image.id} className='grid-images-item'>
       <img src={image.webformatURL} alt='' />
+      <div className='overlay'>
+        <AiOutlineLike />
+        <AiOutlineCloudDownload />
+      </div>
     </div>
   );
 };
